36f39e30e505a5163e7eb7538d0bad756b133625
chujo_yuka <chujo_yuka@soarsystem.co.jp>
2024-08-22 11:42:09 +0900

    kadai5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -27,4 +27,26 @@ class PostController extends Controller
         // posts.showに$productを渡す
         return view('posts.show' , compact('product'));
     }
+
+    public function create() {
+        return view('posts.create');
+    }
+
+    public function store(Request $request) {
+
+        $title = $request->input('title');
+        $content = $request->input('content');
+
+        $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:200'
+        ]);
+
+        $post = new Product();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+
+        return redirect("/posts");
+    }
 }
